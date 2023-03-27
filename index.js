0d25eb195500af631be27a283482b19ed9ac68e8
waltersimekha <waltersimekha@gmail.com>
2023-03-27 13:25:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    var left = parseInt(dodger.style.left)
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    var left = parseInt(dodger.style.left)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
